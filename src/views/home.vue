e7603dfcd6378971adef98bb511a16dbb2638585
JIAFENG1231 <179095019@qq.com>
2023-08-13 22:45:23 +0800

    first commit

---
+++ src/views/home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div w-full h-full>
+        <div class="flex flex-col items-center justify-center h-full">
+            <h1 class="text-4xl font-bold">Home</h1>
+            <p class="text-xl">Welcome to your Vue 3 + Vite + Tailwind CSS app</p>
+        </div>
+    </div>
+</template>
